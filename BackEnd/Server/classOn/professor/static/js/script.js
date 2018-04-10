e9e9fb2773cc84d7024eb61437921f75a574808c
JorgeFrias <100317118@alumnos.uc3m.es>
2018-04-10 15:47:15 +0200

    Sigue sin funcionar.

--- BackEnd/Server/classOn/professor/static/js/script.js
+++ BackEnd/Server/classOn/professor/static/js/script.js
@@ -1,15 +1,18 @@
 socket.on('joinedGroup', function(group){
     var groupJson = JSON.parse(group);                          // To JSON
-    var element = document.getElementById(groupJson.position);
+//    var element = document.getElementById(groupJson.position);
+//    $element = $('#'+groupJson.position)
     // Members
-    var elem = element.getElementById("noMembers");     // Remove "No members"
-    elem.parentNode.removeChild(elem);
+//    var elem = element.getElementById("noMembers");     // Remove "No members"
+//    elem.parentNode.removeChild(elem);
 
-    var students = groupJson.students.data;
-    for (var student in students)
+    $("#noMembers"+groupJson.position).remove();       // Remove no members
+
+    var students = groupJson.students;
+    for (var i in students)
     {
-        var studentHTML = '<li className=\"list-group-item\" id=\"student.db_id\">' + student.name + student.lastname + '</li>';
-        $("members li:last").append(studentHTML);
+        var studentHTML = '<li className=\"list-group-item\" id=\"' + students[i].db_id + '\">' + students[i].name + ' ' + students[i].lastName + '</li>';
+        $("#members li:last").append(studentHTML);
     }
 
     // <div class="card border-secondary" id="{{ row ~ ',' ~ column }}">
