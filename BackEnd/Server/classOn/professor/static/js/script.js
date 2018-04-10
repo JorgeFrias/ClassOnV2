23d1f25d3a1d3b95faeb6f33edfcd734bb1e795b
JorgeFrias <100317118@alumnos.uc3m.es>
2018-04-10 16:50:42 +0200

    Trying to get it to work.

--- BackEnd/Server/classOn/professor/static/js/script.js
+++ BackEnd/Server/classOn/professor/static/js/script.js
@@ -1,31 +1,11 @@
 socket.on('joinedGroup', function(group){
     var groupJson = JSON.parse(group);                          // To JSON
-//    var element = document.getElementById(groupJson.position);
-//    $element = $('#'+groupJson.position)
-    // Members
-//    var elem = element.getElementById("noMembers");     // Remove "No members"
-//    elem.parentNode.removeChild(elem);
-
-    $("#noMembers"+groupJson.position).remove();       // Remove no members
+    $("#noMembers_"+groupJson.position).remove();       // Remove no members
 
     var students = groupJson.students;
     for (var i in students)
     {
         var studentHTML = '<li className=\"list-group-item\" id=\"' + students[i].db_id + '\">' + students[i].name + ' ' + students[i].lastName + '</li>';
-        $("#members li:last").append(studentHTML);
+        $("#members_" + groupJson.position + " li:last").append(studentHTML);
     }
-
-    // <div class="card border-secondary" id="{{ row ~ ',' ~ column }}">
-    //     <div class="card-body">
-    //         <!--<h5 class="card-title" >Progress:</h5>-->
-    //         <h5>
-    //             <span class="badge badge-dark" id="progress">0</span>
-    //             <span class="badge badge-warning" id="doubt">Doubt</span>
-    //         </h5>
-    //     </div>
-    //     <ul class="list-group list-group-flush" id="members">
-    //         <li class="list-group-item" id="noMembers"><small>No members yet</small></li>
-    //     </ul>
-    // </div>
-
 });
